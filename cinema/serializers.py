f46bef362c01b77dd89c1888fc9fc9a60cab8971
ulichka <ulichka7615@gmail.com>
2025-05-12 19:36:12 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -30,10 +30,19 @@ class CinemaHallSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
+        fields = ("id", "image",)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    image = MovieImageSerializer(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "description", "duration", "genres",
+                  "actors", "image")
 
 
 class MovieListSerializer(MovieSerializer):
@@ -51,7 +60,8 @@ class MovieDetailSerializer(MovieSerializer):
 
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
+        fields = ("id", "title", "description", "duration", "genres",
+                  "actors", "image")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
@@ -69,6 +79,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
         source="cinema_hall.capacity", read_only=True
     )
     tickets_available = serializers.IntegerField(read_only=True)
+    movie_image = MovieImageSerializer(source="movie", read_only=True)
 
     class Meta:
         model = MovieSession
@@ -79,6 +90,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "cinema_hall_name",
             "cinema_hall_capacity",
             "tickets_available",
+            "movie_image",
         )
 
 
